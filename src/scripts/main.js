2e6aa413e011f8b033d583a7d5a2c333d109d75d
Natalia Onufrienko <natalia_anamalia@icloud.com>
2026-06-26 13:38:36 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+/* global alert */
+/* eslint-disable no-console */
+
+const form = document.getElementById('subscribeForm');
+
+if (form) {
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const email = form.querySelector('input').value;
+
+    console.log(email);
+    alert('Дякуємо за підписку!');
+  });
+}
