4fdb8296fded9a9f14a20fb3e2b3eee7a82afccd
Natalia Onufrienko <natalia_anamalia@icloud.com>
2026-06-26 10:48:29 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+function toggleMenuLock() {
+  const page = document.querySelector('.page');
+
+  if (window.location.hash === '#menu') {
+    page.classList.add('menu-open');
+  } else {
+    page.classList.remove('menu-open');
+  }
+}
+
+window.addEventListener('hashchange', toggleMenuLock);
+toggleMenuLock();
